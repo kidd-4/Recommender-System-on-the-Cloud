9460d45cfa6adc7bbcf4e08d248fa26d8031fb38
Grey <GreyLee369@gmail.com>
2018-11-20 21:27:28 -0500

    add search movie name class

---
+++ RestfulService/src/object/SimilarMovies.java
@@ -0,0 +1,19 @@
+package object;
+
+public class SimilarMovies {
+	private String moviesName;
+	
+	public SimilarMovies(String moviesName){
+		this.moviesName = moviesName;
+	}
+
+	public String getMoviesName() {
+		return moviesName;
+	}
+
+	public void setMoviesName(String moviesName) {
+		this.moviesName = moviesName;
+	}
+
+	
+}
